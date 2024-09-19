e86827748188867449db27c7e63fb63f60ab375b
shruti ranjan <ranjanshruti186@gmail.com>
2024-09-19 13:17:20 +0530

    Add Terraform configuration to create EC2 instance and install Nginx

---
+++ main.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform_key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "terraform_sg" {
+  name        = "terraform_sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "terraform_instance" {
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.terraform_key.key_name
+  security_groups = [aws_security_group.terraform_sg.name]
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install nginx -y",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+      timeout     = "2m"
+      agent       = false
+    }
+  }
+
+  tags = {
+    Name = "TerraformEC2Instance"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.terraform_instance.public_ip
+}
